8b8ed5e7e2e2b72bb6aac8196eeef71d9698eed7
Faizu Rahman <www.faizu9264@gmail.com>
2025-08-25 18:47:31 +0530

    add to Home screen PWA for mobile app installation

---
+++ next.config.js
@@ -0,0 +1,15 @@
+import nextPwa from 'next-pwa';
+
+const withPWA = nextPwa({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({
+  // ...other Next.js config options
+});
+
+export default nextConfig;
